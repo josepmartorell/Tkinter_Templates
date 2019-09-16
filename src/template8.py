188192c6afe0b022348cb12aa657c5fd7284f648
jmartorell <martorelljosep@gmail.com>
2019-09-16 18:59:10 +0200

    update template8

--- src/template8.py
+++ src/template8.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+""" Tkinter_GUI_Templates:
+    Template 8 consists of a small control selector panel with some basic design features."""
+
 import tkinter as tk
 from tkinter import ttk
 
@@ -8,25 +12,36 @@ class Application(ttk.Frame):
 
     def __init__(self, main_window):
         super().__init__(main_window)
-        main_window.title("Panel de pestañas en Tcl/Tk")
+        if __name__ == '__main__':
+            main_window.title("Position elements in Tcl/Tk")
 
-        # Crear el panel de pestañas.
+        # create notebook
         self.notebook = ttk.Notebook(self)
 
-        # Crear el contenido de cada una de las pestañas.
-        self.web_label = ttk.Label(self.notebook,
-                                   text="www.recursospython.com")
-        self.forum_label = ttk.Label(self.notebook,
-                                     text="foro.recursospython.com")
+        # create content notebook
+        self.tab1_label = ttk.Label(self.notebook,
+                                    text="Write description for the tab 1")
+        # self.tab1_image = tk.PhotoImage(file="images.png")
+        self.tab2_label = ttk.Label(self.notebook,
+                                    text="Write description for the tab 2")
+        self.tab3_label = ttk.Label(self.notebook,
+                                    text="Write description for the tab 3")
+        self.tab4_label = ttk.Label(self.notebook,
+                                    text="Write description for the tab 4")
 
-        # Añadirlas al panel con su respectivo texto.
-        self.notebook.add(self.web_label, text="Web", padding=20)
-        self.notebook.add(self.forum_label, text="Foro", padding=20)
+
+        # add them to the notebook
+        self.notebook.add(self.tab1_label, text="Tab_one", padding=20)
+        # self.notebook.add(self.tab1_label, text="Tab_one", image=self.tab1_image, compound=tk.LEFT, padding=20)
+        self.notebook.add(self.tab2_label, text="Tab_two ", padding=20)
+        self.notebook.add(self.tab3_label, text="Tab_three ", padding=20)
+        self.notebook.add(self.tab4_label, text="Tab_four", padding=20)
 
         self.notebook.pack(padx=10, pady=10)
         self.pack()
 
 
-main_window = tk.Tk()
-app = Application(main_window)
-app.mainloop()
+root = tk.Tk()
+root.resizable(width=False, height=False)
+app = Application(root)
+root.mainloop()
