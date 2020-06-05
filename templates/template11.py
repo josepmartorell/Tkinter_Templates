a89f5ec90fc7761d01155c2af66c4d6ab758fede
josepmartorell <martorelljosep>
2020-06-05 14:53:32 +0200

    search engine design

--- templates/template11.py
+++ templates/template11.py
@@ -25,8 +25,8 @@ if switch != 1:
 else:
 
     def center(tk):
-        w = 1200
-        h = 340
+        w = 1100
+        h = 373
 
         sw = tk.winfo_screenwidth()
         sh = tk.winfo_screenheight()
@@ -43,29 +43,54 @@ else:
             self.master = master
             self.make_widgets()
             master.configure(background='white')
+            master.resizable(width=False, height=False)
 
         def make_widgets(self):
-            self.winfo_toplevel().title("Factiva's Search Engine")
+            self.winfo_toplevel().title('JTECH search engine panel (www.jtech.network)')
 
             # create buttons
-            buttons = ['CHECK BACK',
-                       'CAPTIONS HINT',
-                       'SELECT TARGET',
-                       'SEARCH ENGINE',
-                       'AUTOMATIC TRACKER',
-                       'RECALL POINT',
-                       'JOURNAL FEED',
-                       'MAKE REPORT',
-                       'RESET ALL',
-                       'START/STOP',
-                       'METRIC DISPLAYER']
+            buttons = [
+                'SEARCH ENGINE',
+                'SELECT TARGET',
+                'JOURNAL FEED',
+                'AUTOMATIC TRACKER',
+                'LAUNCHER',
+                'RECALL POINT',
+                'CHECK BACK',
+                'RESET ALL',
+                'DISPLAY METER',
+                'MAKE REPORT',
+                'CAPTIONS HINT',
+
+            ]
 
             row = 0
             for button in buttons:
-                tk.Button(text=button, command=self.output, fg="black", bg="white", relief=tk.RIDGE, width=20).grid(row=row, column=0)
+                tk.Button(text=button, command=self.output, fg="black", bg="white", relief=tk.RIDGE, width=20).grid(
+                    row=row, column=0)
+                row = row + 1
+            # START/STOP
+            tk.Button(text='START/STOP', command=self.output, fg="black", bg="white", relief=tk.RIDGE, width=20).grid(
+                row=11, column=0)
+
+            # create labels + text fields
+            colours = ['azure', 'azure', 'azure', 'azure']
+            row = 0
+            for colour in colours:
+                tk.Entry(bg=colour, relief=tk.SUNKEN, width=80).grid(row=row, column=1)
+                tk.Label(text=' * SET OFF last launchment <time>', bg='white').grid(row=row, column=2, sticky='w')
                 row = row + 1
 
-            # tk.Entry(bg="gray", relief=tk.SUNKEN, width=10).grid(row=0, column=1)
+            # create tracker
+            tracks = [' -> Track 1: deactivated at <time>',
+                      ' -> Track 2: deactivated at <time>',
+                      ' -> Track 3: deactivated at <time>',
+                      ' -> Track 4: deactivated at <time>']
+            row = 4
+            for track in tracks:
+                tk.Label(text=track, fg='blue', bg='white').grid(row=row, column=2, sticky='w')
+                row = row + 1
+
 
         def output(self):
             print("chévere!")
